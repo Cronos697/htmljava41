29cc8633a037f5feb242defef99822430ae3fb05
Cronos697 <colbyjustin1@gmail.com>
2020-11-15 09:04:06 -0500

    End of Class Week

---
+++ Week6Lab/RPS/rps.js
@@ -0,0 +1,45 @@
+var rps = [];
+rps[0] = "Rock";
+rps[1] = "Paper";
+rps[2] = "Scissors";
+
+//array of buttons
+var btn = document.querySelectorAll('a');
+//assign event listeners to the buttons
+btn[0].addEventListener('click', function(e){play(0)})
+btn[1].addEventListener('click', function(e){play(1)})
+btn[2].addEventListener('click', function(e){play(2)})
+
+function play(playersChoice){
+    var cpuChoice = Math.floor(Math.random() *2.999);
+    alert("Player Choice: " + rps[playersChoice] + " vs. Computer Choice: " + rps[cpuChoice]);
+switch(playersChoice){
+    case 0:
+        if(cpuChoice === 0){
+alert("It's a tie!")
+        }
+        else if(cpuChoice === 1){
+alert("You Lost !")
+        }
+        else{
+            alert("You Win")
+        }
+         break;
+    case 1:
+        if(cpuChoice === 0){
+            alert("It's a tie!")
+                    }
+                    else if(cpuChoice === 1){
+            alert("You Lost !")
+                    }
+                    else{
+                        alert("You Win")
+                    }
+        
+         break;
+    case 2:
+         break;
+}
+
+
+}
